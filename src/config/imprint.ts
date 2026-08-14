521bc5741028538147a36d5af12f101c506c10e8
hpmine <hpmine@web.de>
2026-08-14 23:56:07 +0200

    Update imprint configuration with personal details

--- src/config/imprint.ts
+++ src/config/imprint.ts
@@ -9,18 +9,18 @@
  * text checked if you are unsure which information applies to you.
  */
 export const imprintConfig = {
-  providerName: '[Vor- und Nachname oder vollständiger Firmenname]',
+  providerName: '[Jakob Gregory]',
 
   address: {
-    street: '[Straße und Hausnummer]',
-    postalCode: '[PLZ]',
-    city: '[Ort]',
+    street: '[Schwalbstraße 5]',
+    postalCode: '[53332]',
+    city: '[Bornheim]',
     country: 'Deutschland',
   },
 
   contact: {
-    email: '[deine E-Mail-Adresse]',
-    phone: '[deine Telefonnummer]',
+    email: '[hpmine@web.de]',
+    phone: '[]',
   },
 
   // For companies or organizations, for example: "Max Mustermann"
